f85e3516377a7e60f519ad22995cf76e8360b41b
Skullywag <mjskull@gmail.com>
2015-09-02 17:44:12 +0100

    updated to a12, setup gitignore

--- Source/MedievalShields/Apparel_Shield.cs
+++ Source/MedievalShields/Apparel_Shield.cs
@@ -13,12 +13,13 @@ namespace MedievalShields
         public static readonly SoundDef SoundAbsorbDamage = SoundDef.Named("PersonalShieldAbsorbDamage");
         public static readonly SoundDef SoundBreak = SoundDef.Named("PersonalShieldBroken");
         public Vector3 impactAngleVect;
+        public ThingDef equippedDef;
         
         public bool ShouldDisplay
         {
             get
             {
-                return !this.wearer.Dead && !this.wearer.Downed && (!this.wearer.IsPrisonerOfColony || (this.wearer.BrokenStateDef != null && this.wearer.BrokenStateDef == BrokenStateDefOf.Berserk)) && ((this.wearer.playerController != null && this.wearer.playerController.Drafted) || this.wearer.Faction.HostileTo(Faction.OfColony));
+                return !this.wearer.Dead && !this.wearer.InBed() && !this.wearer.Downed && (!this.wearer.IsPrisonerOfColony || (this.wearer.BrokenStateDef != null && this.wearer.BrokenStateDef == BrokenStateDefOf.Berserk)) || this.wearer.Faction.HostileTo(Faction.OfColony);
             }
         }
         public override void ExposeData()
@@ -41,7 +42,7 @@ namespace MedievalShields
                 {
                     System.Random random1 = new System.Random();
                     int chance1 = random1.Next(0, 5);
-                    if (chance1 == 1)
+                    if (chance1 >= 3)
                     {
                         this.HitPoints -= dinfo.Amount / 2;
                         if (this.HitPoints <= 0)
@@ -59,7 +60,7 @@ namespace MedievalShields
                 {
                     System.Random random2 = new System.Random();
                     int chance2 = random2.Next(0, 5);
-                    if (chance2 >= 3)
+                    if (chance2 >= 2)
                     {
                         this.HitPoints -= dinfo.Amount / 4;
                         if (this.HitPoints <= 0)
@@ -90,51 +91,67 @@ namespace MedievalShields
         }
         public override bool AllowVerbCast(IntVec3 root, TargetInfo targ)
         {
-            return true;
+            equippedDef = this.wearer.equipment.Primary.def;
+            if (equippedDef.IsMeleeWeapon)
+            {
+                return true;
+            }
+            if (equippedDef.defName.Contains("pistol") || equippedDef.defName.Contains("Pistol"))
+            {
+                return true;
+            }
+            if (equippedDef == ThingDef.Named("Gun_PDW"))
+            {
+                return true;
+            }
+            return false;
         }
         public override void DrawWornExtras()
         {
-            float num = 0f;
-            Vector3 vector = this.wearer.drawer.DrawPos;
-            vector.y = Altitudes.AltitudeFor(AltitudeLayer.Pawn);
-            Vector3 s = new Vector3(1f, 1f, 1f);
-            if (this.wearer.Rotation == Rot4.North)
+            if (this.ShouldDisplay)
             {
+                float num = 0f;
+                Vector3 vector = this.wearer.drawer.DrawPos;
                 vector.y = Altitudes.AltitudeFor(AltitudeLayer.Pawn);
-                vector.x -= 0.2f;
-                vector.z -= 0.2f;
-            }
-            else
-            {
-                if (this.wearer.Rotation == Rot4.South)
+                Vector3 s = new Vector3(1f, 1f, 1f);
+                if (this.wearer.Rotation == Rot4.North)
                 {
-                    vector.y = Altitudes.AltitudeFor(AltitudeLayer.MoteOverhead);
-                    vector.x += 0.2f;
+                    vector.y = Altitudes.AltitudeFor(AltitudeLayer.Pawn);
+                    vector.x -= 0.2f;
                     vector.z -= 0.2f;
                 }
                 else
                 {
-                    if (this.wearer.Rotation == Rot4.East)
+                    if (this.wearer.Rotation == Rot4.South)
                     {
-                        vector.y = Altitudes.AltitudeFor(AltitudeLayer.Pawn);
+                        vector.y = Altitudes.AltitudeFor(AltitudeLayer.MoteOverhead);
+                        vector.x += 0.2f;
                         vector.z -= 0.2f;
-                        num = 90f;
                     }
                     else
                     {
-                        if (this.wearer.Rotation == Rot4.West)
+                        if (this.wearer.Rotation == Rot4.East)
                         {
-                            vector.y = Altitudes.AltitudeFor(AltitudeLayer.MoteOverhead);
+                            vector.y = Altitudes.AltitudeFor(AltitudeLayer.Pawn);
                             vector.z -= 0.2f;
-                            num = 270f;
+                            num = 90f;
+                        }
+                        else
+                        {
+                            if (this.wearer.Rotation == Rot4.West)
+                            {
+                                vector.y = Altitudes.AltitudeFor(AltitudeLayer.MoteOverhead);
+                                vector.z -= 0.2f;
+                                num = 270f;
+                            }
                         }
                     }
                 }
+                Matrix4x4 matrix = default(Matrix4x4);
+                matrix.SetTRS(vector, Quaternion.AngleAxis(num, Vector3.up), s);
+                shieldMat = MaterialPool.MatFrom("Things/Item/Equipment/Apparel/Accessory/Shield", ShaderDatabase.Cutout, this.Stuff.stuffProps.color);
+                Graphics.DrawMesh(MeshPool.plane10, matrix, shieldMat, 0);
             }
-            Matrix4x4 matrix = default(Matrix4x4);
-            matrix.SetTRS(vector, Quaternion.AngleAxis(num, Vector3.up), s);
-            shieldMat = MaterialPool.MatFrom("Things/Item/Equipment/Apparel/Accessory/Shield", ShaderDatabase.Cutout, this.Stuff.stuffProps.color);
-            Graphics.DrawMesh(MeshPool.plane10, matrix, shieldMat, 0);
         }
     }
 }
